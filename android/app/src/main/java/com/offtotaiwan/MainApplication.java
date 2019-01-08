c5ba8575f493dd9862e03f76a5c02cf0dbcbeea4
Alice Hsiao <alice.hsiao87@gmail.com>
2019-01-08 08:15:50 -0800

    Implemented React Native Navigation using Podfile

--- android/app/src/main/java/com/offtotaiwan/MainApplication.java
+++ android/app/src/main/java/com/offtotaiwan/MainApplication.java
@@ -3,6 +3,10 @@ package com.offtotaiwan;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +29,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new MapsPackage(),
+          new RNGestureHandlerPackage(),
+          new VectorIconsPackage(),
           new MapsPackage()
       );
     }
